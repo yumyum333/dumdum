bfa6f1f3dd2599ff57de3e15bd1710dcbedea48e
yumyum333 <aenguswoo@gmail.com>
2024-04-17 14:02:34 +0100

    chrome?

--- create_instance.py
+++ create_instance.py
@@ -3,6 +3,9 @@ import os
 from dotenv import load_dotenv
 import time
 import botocore
+import random
+from datetime import datetime
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -12,6 +15,8 @@ def launch_instances(image_id, instance_type, vpn_count, non_vpn_count, key_name
     # security_group_id = SECURITY_GROUP_IDS[0]
 
     ec2 = boto3.client('ec2', region_name=region_name)
+
+    
 
     # Check if the rule already exists
     try:
@@ -43,6 +48,7 @@ def launch_instances(image_id, instance_type, vpn_count, non_vpn_count, key_name
     # Launch VPN instances
     if vpn_count > 0:
         try:
+            exp_time = datetime.now().strftime("%b%d-%H%M-%S")
             vpn_response = ec2.run_instances(
                 ImageId=image_id,
                 InstanceType=instance_type,
@@ -50,7 +56,18 @@ def launch_instances(image_id, instance_type, vpn_count, non_vpn_count, key_name
                 MaxCount=vpn_count,
                 KeyName=key_name,
                 SecurityGroupIds=[security_group_id],
-                UserData=vpn_user_data
+                UserData=vpn_user_data,
+                TagSpecifications=[
+                    {
+                        'ResourceType': 'instance',
+                        'Tags': [
+                            {
+                                'Key': 'Name',
+                                'Value': f'goose-{exp_time}'
+                            }
+                        ]
+                    }
+                ]
             )
             print(f"Successfully launched {vpn_count} VPN instances.")
         except Exception as e:
@@ -66,7 +83,18 @@ def launch_instances(image_id, instance_type, vpn_count, non_vpn_count, key_name
                 MaxCount=non_vpn_count,
                 KeyName=key_name,
                 SecurityGroupIds=[security_group_id],
-                UserData=non_vpn_user_data
+                UserData=non_vpn_user_data,
+                TagSpecifications=[
+                    {
+                        'ResourceType': 'instance',
+                        'Tags': [
+                            {
+                                'Key': 'Name',
+                                'Value': f'goose-{exp_time}'
+                            }
+                        ]
+                    }
+                ]
             )
             print(f"Successfully launched {non_vpn_count} non-VPN instances.")
         except Exception as e:
@@ -154,32 +182,35 @@ if __name__ == "__main__":
     COMMON_USER_DATA_1 = rf"""<powershell>
     # Create a DEBUG file on the Desktop
     New-Item -Path 'C:\Users\Administrator\Desktop' -Name 'DEBUG_HERE' -ItemType 'file' -Force
-    
-    # Open Microsoft Edge
-    Start-Process "msedge"
-    
+
     # Set the Administrator password
     net user Administrator "{ADMIN_PASSWORD}"
 
+    # Install Google Chrome
+    $Path = 'C:\Users\Administrator\Desktop\chrome_installer.exe'
+    Invoke-WebRequest 'https://dl.google.com/chrome/install/standalone/ChromeStandaloneSetup64.exe' -OutFile $Path
+    Start-Process -FilePath $Path -Args '/silent /install' -Wait
+    Remove-Item $Path
+
     # Install Python and virtual environment tools
-    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe" -OutFile "python-installer.exe"
-    Start-Process -FilePath "python-installer.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
-    Remove-Item "python-installer.exe"
+    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe" -OutFile "C:\Users\Administrator\Desktop\python-installer.exe"
+    Start-Process -FilePath "C:\Users\Administrator\Desktop\python-installer.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1" -Wait
+    Remove-Item "C:\Users\Administrator\Desktop\python-installer.exe"
 
     # Create a Python virtual environment and install dependencies
-    python -m venv C:\venv
-    C:\venv\Scripts\activate.ps1
+    python -m venv C:\Users\Administrator\Desktop\venv
+    C:\Users\Administrator\Desktop\venv\Scripts\activate.ps1
     python -m pip install --upgrade pip
     """
 
     COMMON_USER_DATA_2 = rf"""
-    # Download the Python script and requirements.txt (Assuming they are available via a public URL)
-    Invoke-WebRequest -Uri "https://github.com/yumyum333/dumdum/blob/main/monitor_website.py" -OutFile "C:\\monitor_website.py"
-    Invoke-WebRequest -Uri "https://github.com/yumyum333/dumdum/blob/main/requirements.txt" -OutFile "C:\\requirements.txt"
-    pip install -r C:\\requirements.txt
+    # Download the Python script and requirements.txt to the Desktop
+    Invoke-WebRequest -Uri "https://github.com/yumyum333/dumdum/blob/main/monitor_website.py" -OutFile "C:\Users\Administrator\Desktop\monitor_website.py"
+    Invoke-WebRequest -Uri "https://github.com/yumyum333/dumdum/blob/main/requirements.txt" -OutFile "C:\Users\Administrator\Desktop\requirements.txt"
+    pip install -r C:\Users\Administrator\Desktop\requirements.txt
 
     # Run the Python script with arguments 
-    python C:\\monitor_website.py --url {URL} --interval {INTERVAL} --source-email {SOURCE_EMAIL} --destination-email {DESTINATION_EMAIL} --email-password {EMAIL_PASSWORD}
+    python C:\Users\Administrator\Desktop\monitor_website.py --url {URL} --interval {INTERVAL} --source-email {SOURCE_EMAIL} --destination-email {DESTINATION_EMAIL} --email-password {EMAIL_PASSWORD}
     </powershell>
     """
 
